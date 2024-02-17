77d0ca588bfe4286c08c5a2ef660adf57132d9d9
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2024-02-17 16:23:33 +0000

    Add vendor.sh

---
+++ README.md
@@ -0,0 +1,13 @@
+# Generate-DockerImageVariantsTemplates
+
+Templates for [Generate-DockerImageVariants](https://github.com/theohbrothers/Generate-DockerImageVariants) repositories.
+
+## Usage
+
+```sh
+# Vendor all files into current working directory
+./vendor.sh --dry-run
+
+# Vendor CI files into current working directory
+./vendor.sh ci --dry-run
+```
---
+++ vendor.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+set -eu
+
+SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd )
+TARGET_DIR="$PWD"
+
+usage() {
+    echo "Rsync templates to your repo"
+    echo "Usage: vendor.sh [--help]"
+    echo "Examples:"
+    echo "  # Vendor all templates to the current working directory (dry-run)"
+    echo "  /path/to/Generate-DockerImageVariantsTemplates/vendor.sh -n"
+    echo
+    echo "  # Vendor CI templates to the current working directory"
+    echo "  /path/to/Generate-DockerImageVariantsTemplates/vendor.sh"
+
+    echo "  # Vendor CI templates to the current working directory"
+    echo "  /path/to/Generate-DockerImageVariantsTemplates/vendor.sh ci"
+}
+
+# Exit if we got no options
+# if [ $# -eq 0 ]; then usage; exit 1; fi
+
+# Get some options
+while test $# -gt 0; do
+    case "$1" in
+        -h|--help)
+            usage
+            exit 0
+            ;;
+        -n|--dry-run)
+            DRY_RUN=1
+            shift
+            ;;
+        ci)
+            SUBCOMMAND_CI=1
+            shift
+            ;;
+        *)
+            echo "Invalid option '$1'" 1>&2
+            usage
+            exit 1
+            ;;
+    esac
+done
+
+SUBCOMMAND_CI=${SUBCOMMAND_CI:-}
+DRY_RUN=${DRY_RUN:-}
+
+if [ -n "$DRY_RUN" ]; then
+    set -- '-n'
+fi
+
+if [ "$SUBCOMMAND_CI" ]; then
+    (
+        cd "$SCRIPT_DIR"
+        rsync -av .github .vscode generate LICENSE Update-Versions.ps1 "$TARGET_DIR" --exclude=generate/definitions --exclude='generate/templates/*.ps1' "$@"
+    )
+else
+    (
+        cd "$SCRIPT_DIR"
+        rsync -av .github .vscode generate LICENSE Update-Versions.ps1 "$TARGET_DIR" "$@"
+    )
+fi
